1169d4fbc731615fab74cb0ca0a0c0249061a4de
Yassine <yassine.tihmmou@gmail.com>
2025-03-07 21:42:37 +0000

    updated to laravel 11

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\MyAccountController;
+use App\Http\Controllers\Admin\AdminHomeController;
+use App\Http\Controllers\Admin\AdminProductController;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index'])->name("home.index");
+Route::get('/about', [HomeController::class, 'about'])->name("home.about");
+Route::get('/products', [ProductController::class, 'index'])->name("product.index");
+Route::get('/products/{id}', [ProductController::class, 'show'])->name("product.show");
+
+Route::get('/cart', [CartController::class, 'index'])->name("cart.index");
+Route::get('/cart/delete', [CartController::class, 'delete'])->name("cart.delete");
+Route::post('/cart/add/{id}', [CartController::class, 'add'])->name("cart.add");
+
+Route::middleware('auth')->group(function () {
+    Route::get('/cart/purchase', [CartController::class, 'purchase'])->name("cart.purchase");
+    Route::get('/my-account/orders', [MyAccountController::class, 'orders'])->name("myaccount.orders");
+});
+
+Route::middleware('admin')->group(function () {
+    Route::get('/admin', [AdminHomeController::class, 'index'])->name("admin.home.index");
+    Route::get('/admin/products', [AdminProductController::class, 'index'])->name("admin.product.index");
+    Route::post('/admin/products/store', [AdminProductController::class, 'store'])->name("admin.product.store");
+    Route::delete('/admin/products/{id}/delete', [AdminProductController::class, 'delete'])->name("admin.product.delete");
+    Route::get('/admin/products/{id}/edit', [AdminProductController::class, 'edit'])->name("admin.product.edit");
+    Route::put('/admin/products/{id}/update', [AdminProductController::class, 'update'])->name("admin.product.update");
+});
+
+Auth::routes();
